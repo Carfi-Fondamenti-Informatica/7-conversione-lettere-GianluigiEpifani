37b48f1278fd6c8ad2aa3e1fc7e45f4bf4fb008e
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-05-03 20:19:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-
-int main(){
-    char c;
-    cin >> c;
-    int n= (int)c;
-    bool x=caratteri(n);
-
-    if (x==1){
-        if ((65 <= n)&&(n <= 90)){
-            n=n+32;
-            cout << (char)n << endl;
-        } else if ((97 <= n)&&(n <= 122)){
-            n=n-32;
-            cout << (char)n << endl;
-        }
-    } else {
-        cout << "errore" << endl;
-    }
-
+int main() {
+  char a;
+  cin>>a;
+  if(controllo(a))
+  {
+   cout<<a;
+  }
+  else
+  {
+      cout<<"errore";
+  }
     return 0;
 }
